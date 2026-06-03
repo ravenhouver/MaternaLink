329e131767d5ab9e5723ac02be47e063e200f41a
0xSyam <0xSyam@pm.me>
2026-06-03 22:26:11 +0700

    feat: bootstrap nest swagger api

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('MaternaLink API')
+    .setDescription('Normalized MaternaLink REST API')
+    .setVersion('0.1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(Number(process.env.PORT ?? 3000));
+}
+
+void bootstrap();
